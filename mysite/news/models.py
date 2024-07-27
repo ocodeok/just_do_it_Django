29fff177889bd0903c4836002dc19b5c01a42664
Ivan Ryzhenkov <ocodeok@gmail.com>
2024-07-27 19:34:26 +0300

    5. Внешний вид шаблона

--- mysite/news/models.py
+++ mysite/news/models.py
@@ -5,10 +5,13 @@ class News(models.Model):
     title = models.CharField(max_length=120, verbose_name="Название новости")
     content = models.TextField(blank=True, verbose_name="Контент")
     created_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата публикации")
-    updated_at = models.DateTimeField(auto_now=True , verbose_name="Фото")
+    updated_at = models.DateTimeField(auto_now=True, verbose_name="Фото")
     photo = models.ImageField(upload_to='photos/%Y/%m/%d/', verbose_name="Фото", blank=True)
     is_published = models.BooleanField(default=False, verbose_name="Опубликованно")
     category = models.ForeignKey("Category", on_delete=models.PROTECT, null=True, verbose_name="Категория")
+
+    def __str__(self):
+        return f"Новость: {self.title}"
 
     class Meta:
         verbose_name = 'Новость'
@@ -18,6 +21,9 @@ class News(models.Model):
 
 class Category(models.Model):
     title = models.CharField(max_length=125, db_index=True, verbose_name="Наименование категории")
+
+    def __str__(self):
+        return f"Категория {self.title}"
 
     class Meta:
         verbose_name = "Категория"
