252d3f3dabf734181cfbd7baee01dab10e13daa9
Ivan Ryzhenkov <ocodeok@gmail.com>
2024-07-27 11:51:18 +0300

    4. Связи моделей

--- mysite/news/models.py
+++ mysite/news/models.py
@@ -8,8 +8,18 @@ class News(models.Model):
     updated_at = models.DateTimeField(auto_now=True , verbose_name="Фото")
     photo = models.ImageField(upload_to='photos/%Y/%m/%d/', verbose_name="Фото", blank=True)
     is_published = models.BooleanField(default=False, verbose_name="Опубликованно")
+    category = models.ForeignKey("Category", on_delete=models.PROTECT, null=True, verbose_name="Категория")
 
     class Meta:
         verbose_name = 'Новость'
         verbose_name_plural = 'Новости'
         ordering = ['-created_at', 'title']
+
+
+class Category(models.Model):
+    title = models.CharField(max_length=125, db_index=True, verbose_name="Наименование категории")
+
+    class Meta:
+        verbose_name = "Категория"
+        verbose_name_plural = "Категории"
+        ordering = ['title']
