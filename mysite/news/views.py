d2428c59aa4d6e3d1abeacb0bf6938b7b1bdbf5c
Ivan Ryzhenkov <ocodeok@gmail.com>
2024-08-02 19:39:55 +0300

    Работа с формами. Часть 2

--- mysite/news/views.py
+++ mysite/news/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 
 from .models import News, Category
 from .forms import NewsForm
@@ -31,7 +31,10 @@ def view_news(request, news_id):
 
 def add_news(request):
     if request.method == "POST":
-        pass
+        form = NewsForm(request.POST)
+        if form.is_valid():
+            news = News.objects.create(**form.cleaned_data)
+            return redirect(news)
     else:
         form = NewsForm()
 
