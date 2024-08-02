fe84a71757028a32c5aa6dcbb7596484c790ead7
Ivan Ryzhenkov <ocodeok@gmail.com>
2024-08-02 13:53:41 +0300

    Работа с формами. Часть 1

--- mysite/news/views.py
+++ mysite/news/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 
 from .models import News, Category
+from .forms import NewsForm
 
 
 def get_index(request):
@@ -25,4 +26,15 @@ def view_news(request, news_id):
     news_item = get_object_or_404(News, pk=news_id)
     return render(request,
                   'news/view_news.html',
-                  {'news_item': news_item})  
+                  {'news_item': news_item})
+
+
+def add_news(request):
+    if request.method == "POST":
+        pass
+    else:
+        form = NewsForm()
+
+    return render(request,
+                  'news/add_news.html',
+                  {"form": form})
