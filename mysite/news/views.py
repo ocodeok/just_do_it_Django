67b09cde2df2d2ec897c3464ca97652b3bb20c6d
Ivan Ryzhenkov <ocodeok@gmail.com>
2024-08-22 16:43:22 +0300

    Класс CreateView

--- mysite/news/views.py
+++ mysite/news/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404, redirect
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView
+from django.urls import reverse_lazy
 
 from .models import News, Category
 from .forms import NewsForm
@@ -17,6 +18,12 @@ class HomeView(ListView):
 
     def get_queryset(self):
         return News.objects.filter(is_published=True)
+
+
+class CreateNews(CreateView):
+    form_class = NewsForm
+    template_name = 'news/add_news.html'
+    success_url = reverse_lazy('home')
 
 
 class NewsByCategory(ListView):
@@ -40,46 +47,5 @@ class ViewNews(DetailView):
     model = News
     template_name = 'news/news_detail.html'
     context_object_name = 'news_item'
-    # pk_url_kwarg = 'news_id'
+    # pk_url_kwarg = 'news_id' // Именованный параметр принимаемый в urlpatterns
 
-
-
-# def get_index(request):
-#     news = News.objects.order_by('-created_at')
-#
-#     context = {'news': news,
-#                'title': 'Список новостей'}
-#     return render(request,
-#                   'news/index.html', context)
-
-
-# def get_category(request, category_id):
-#     news = News.objects.filter(category_id=category_id)
-#     category = Category.objects.get(id=category_id)
-#
-#     return render(request,
-#                   'news/category.html',
-#                   {'news': news})
-
-
-# def view_news(request, news_id):
-#     news_item = get_object_or_404(News, pk=news_id)
-#     return render(request,
-#                   'news/view_news.html',
-#                   {'news_item': news_item})
-
-
-def add_news(request):
-    if request.method == "POST":
-        form = NewsForm(request.POST)
-        if form.is_valid():
-            # news = News.objects.create(**form.cleaned_data)
-            news = form.save()
-            return redirect(news)
-
-    else:
-        form = NewsForm()
-
-    return render(request,
-                  'news/add_news.html',
-                  {"form": form})
